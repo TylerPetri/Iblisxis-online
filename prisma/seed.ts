8fc56b3fa2ee38796177e8e1b9fd785a5a94fefd
tyler <tylerpetri93@gmail.com>
2024-10-30 16:19:18 -0400

    resolved schema conflicts, but prisma doesnt support checks nor geometry types, so now looking for another orm

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,24 +2,24 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
   const jabami = await prisma.players.upsert({
-    where: { email: 'jabami@prisma.io' },
+    where: { name: 'Jabami' },
     update: {},
     create: {
-      email: 'alice@prisma.io',
+      email: 'jabami@prisma.io',
       name: 'Jabami',
     },
   })
   const kuroko = await prisma.players.upsert({
-    where: { email: 'kuroko@prisma.io' },
+    where: { name: 'Kuroko' },
     update: {},
     create: {
-      email: 'bob@prisma.io',
+      email: 'kuroko@prisma.io',
       name: 'Kuroko',
     },
   })
   console.log({ jabami, kuroko })
 
-  const ships = [{ name: 'aircraftCarrier', size: 5, color: 'orange' }, { name: 'battleship', size: 4, color: 'red' }, { name: 'cruiser', size: 3, color: 'grey' }, { name: 'submarine', size: 3, color: 'green' }, { name: 'destroyer', size: 2, color: 'purple' }]
+  const ships = [{ name: 'aircraftCarrier', size: 5, color: 'orange', direction: 'horizontal', row: 3, col: 2 }, { name: 'battleship', size: 4, color: 'red', direction: 'horizontal', row: 1, col: 5 }, { name: 'cruiser', size: 3, color: 'grey', direction: 'horizontal', row: 4, col: 6 }, { name: 'submarine', size: 3, color: 'green', direction: 'horizontal', row: 7, col: 7 }, { name: 'destroyer', size: 2, color: 'purple', direction: 'horizontal', row: 3, col: 8 }]
 
   for (let i = 0; i < ships.length; i++) {
     const ship = await prisma.ships.upsert({
@@ -31,7 +31,23 @@ async function main() {
         color: ships[i].color
       }
     })
-    console.log({ ship })
+
+    const playerShipPlacements = await prisma.player_ship_placements.upsert({
+      where: {
+        player_id: jabami.id,
+        row: ships[i].row,
+        col: ships[i].col
+      },
+      update: {},
+      create: {
+        ship_id: ship.id,
+        row: ships[i].row,
+        col: ships[i].col,
+        player_id: jabami.id,
+        direction: ships[i].direction
+      }
+    })
+    console.log({ ship, playerShipPlacements })
   }
 }
 main()
--- src/lib/server/schema.sql
+++ src/lib/server/schema.sql
@@ -1,44 +1,73 @@
+CREATE EXTENSION postgis;
+
+CREATE TYPE looking_for_game_status_enum AS ENUM ('idle', 'waiting', 'playing');
+CREATE TYPE player_order_enum AS ENUM ('player', 'opponent');
+CREATE TYPE ship_name_enum AS ENUM ('aircraftCarrier', 'battleship', 'cruiser', 'submarine', 'destroyer');
+CREATE TYPE ship_color_enum AS ENUM ('orange', 'red', 'grey', 'green', 'purple');
+CREATE TYPE player_shot_result_enum AS ENUM ('miss', 'hit', 'sunk', 'won', 'ERROR');
+CREATE TYPE game_status_enum AS ENUM ('ongoing', 'finished');
+
+
 CREATE TABLE IF NOT EXISTS players (
 	id serial NOT NULL UNIQUE,
 	name varchar(255) NOT NULL UNIQUE,
 	email varchar(255) UNIQUE,
 	password varchar(255),
+	looking_for_game_status looking_for_game_status_enum NOT NULL DEFAULT 'idle',
 	logged_in boolean NOT NULL DEFAULT false,
 	user_token varchar(255),
 	token_expiration timestamp without time zone,
-	game_status varchar(255) NOT NULL DEFAULT 'idle',
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS matches (
+CREATE TABLE IF NOT EXISTS game_players (
+	game_id bigint NOT NULL,
+	player_id bigint NOT NULL,
+	score bigint NOT NULL,
+	player_order player_order_enum NOT NULL,
+	PRIMARY KEY (game_id, player_id)
+);
+
+CREATE TABLE IF NOT EXISTS lu_ships (
 	id serial NOT NULL UNIQUE,
-	player1_id bigint NOT NULL,
-	player2_id bigint NOT NULL,
+	name ship_name_enum NOT NULL UNIQUE,
+	size bigint NOT NULL CHECK(size <= 5 and size >= 2),
+	color ship_color_enum NOT NULL UNIQUE,
+	points bigint NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS ships (
+CREATE TABLE IF NOT EXISTS game_player_ships (
 	id serial NOT NULL UNIQUE,
-	name varchar(255) NOT NULL UNIQUE,
-	size bigint NOT NULL,
-	color varchar(255) NOT NULL UNIQUE,
+	game_id bigint NOT NULL,
+	player_id bigint NOT NULL,
+	ship_id bigint NOT NULL,
+	line geometry NOT NULL,
+	hits bigint NOT NULL DEFAULT 0,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS player_ship_placements (
+CREATE TABLE IF NOT EXISTS game_player_shots (
 	id serial NOT NULL UNIQUE,
-	ship_id bigint NOT NULL UNIQUE,
-	row bigint NOT NULL,
-	col bigint NOT NULL,
-	player_id bigint NOT NULL UNIQUE,
+	game_id bigint NOT NULL,
+	player_id bigint NOT NULL,
+	point geometry NOT NULL,
+	result player_shot_result_enum NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS games (
+	id serial NOT NULL UNIQUE,
+	winner_player_id bigint,
+	status game_status_enum NOT NULL DEFAULT 'ongoing',
 	PRIMARY KEY (id)
 );
 
 
-ALTER TABLE matches ADD CONSTRAINT matches_fk1 FOREIGN KEY (player1_id) REFERENCES players(id);
+ALTER TABLE game_players ADD CONSTRAINT game_players_fk0 FOREIGN KEY (game_id) REFERENCES games(id);
+ALTER TABLE game_players ADD CONSTRAINT game_players_fk1 FOREIGN KEY (player_id) REFERENCES players(id);
 
-ALTER TABLE matches ADD CONSTRAINT matches_fk2 FOREIGN KEY (player2_id) REFERENCES players(id);
+ALTER TABLE game_player_ships ADD CONSTRAINT game_player_ships_fk1 FOREIGN KEY (game_id, player_id) REFERENCES game_players(game_id, player_id);
+ALTER TABLE game_player_ships ADD CONSTRAINT game_player_ships_fk2 FOREIGN KEY (ship_id) REFERENCES lu_ships(id);
 
-ALTER TABLE player_ship_placements ADD CONSTRAINT player_ship_placements_fk1 FOREIGN KEY (ship_id) REFERENCES ships(id);
-
-ALTER TABLE player_ship_placements ADD CONSTRAINT player_ship_placements_fk4 FOREIGN KEY (player_id) REFERENCES players(id);
+ALTER TABLE game_player_shots ADD CONSTRAINT game_player_shots_fk1 FOREIGN KEY (game_id, player_id) REFERENCES game_players(game_id, player_id);
